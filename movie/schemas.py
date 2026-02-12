ba97be69e27d4d2e1e81f2b72f02b48579f2fa95
dan <danial.hedaiat@gmail.com>
2026-02-12 23:28:37 +0330

    create schema for movie and favorite movie

---
+++ movie/schemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+
+
+class MovieBaseSchema(BaseModel):
+    name: str = Field(..., max_length=100)
+
+
+class MovieCreateSchema(MovieBaseSchema):
+    pass
+
+
+class MovieUpdateSchema(MovieBaseSchema):
+    pass
+
+
+class MovieResponse(MovieBaseSchema):
+    id: int
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class AddFavoriteRequest(BaseModel):
+    user_id: int
+    movie_id: int
