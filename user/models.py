3d815263acbfab0f2ffd5e7aa59e6f27d2a19be3
dan <danial.hedaiat@gmail.com>
2026-02-12 19:26:12 +0330

    update user model

--- user/models.py
+++ user/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
-from sqlalchemy import Column, String, Integer, Boolean, DateTime, func
+from sqlalchemy import Column, String, Integer, Boolean, DateTime, func, text
+from sqlalchemy.orm import relationship
 
 from core.database import Base
 
@@ -11,9 +12,17 @@ class UserModel(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     first_name = Column(String(100), nullable=False)
     last_name = Column(String(100), nullable=False)
-    phone_number = Column(String(15), nullable=False)
-    email = Column(String(100), nullable=False)
-    is_verify = Column(Boolean, default=False)
+
+    phone_number = Column(String(15), nullable=False, unique=True, index=True)
+    email = Column(String(100), nullable=False, unique=True, index=True)
+
+    is_verify = Column(Boolean, nullable=False, server_default=text("false"))
 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+    favorite_movies = relationship(
+        "UserFavoriteMovieModel",
+        back_populates="user",
+        cascade="all, delete"
+    )
