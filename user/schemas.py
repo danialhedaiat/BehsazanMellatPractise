1b37594ae72b6d4ff69d9bfc466195c705b85a60
dan <danial.hedaiat@gmail.com>
2026-02-12 23:28:53 +0330

    create schema for user

---
+++ user/schemas.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from pydantic import BaseModel, EmailStr, Field
+
+
+class UserBaseSchema(BaseModel):
+    first_name: str = Field(..., max_length=100)
+    last_name: str = Field(..., max_length=100)
+    phone_number: str = Field(..., max_length=15)
+    email: EmailStr
+
+
+class UserCreateSchema(UserBaseSchema):
+    pass
+
+
+class UserUpdateSchema(BaseModel):
+    first_name: str | None = None
+    last_name: str | None = None
+    phone_number: str | None = None
+    email: EmailStr | None = None
+    is_verify: bool | None = None
+
+
+class UserResponseSchema(UserBaseSchema):
+    id: int
+    is_verify: bool
+    created_at: datetime
+    updated_at: datetime
+
+    model_config = {
+        "from_attributes": True
+    }
