2b33b30d6f008c00a42db19c763fde77332b14f8
dan <danial.hedaiat@gmail.com>
2026-02-13 19:10:37 +0330

    udpate job model

--- job/models.py
+++ job/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import UniqueConstraint, Column, Integer, String, Date
+from sqlalchemy import UniqueConstraint, Column, Integer, String, DateTime
 
 from core.database import Base
 
@@ -6,11 +6,7 @@ from core.database import Base
 class JobModel(Base):
     __tablename__ = 'jobs'
 
-    __table_args__ = (
-        UniqueConstraint('user_id', 'movie_id', name='unique_user_movie'),
-    )
-
     id = Column(Integer, primary_key=True, autoincrement=True)
     job_name = Column(String(50), nullable=False)
     run_type = Column(String(50), nullable=False)
-    run_time = Column(Date, nullable=False)
+    run_time = Column(DateTime, nullable=False)
