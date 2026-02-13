5244d77459adc2f9b1e5777fff82e15ba5b4c1ad
dan <danial.hedaiat@gmail.com>
2026-02-13 16:20:12 +0330

    add password create to user model

--- user/models.py
+++ user/models.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 
+from passlib.context import CryptContext
 from sqlalchemy import Column, String, Integer, Boolean, DateTime, func, text
 from sqlalchemy.orm import relationship
 
 from core.database import Base
 from movie.models import UserFavoriteMovieModel
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class UserModel(Base):
@@ -16,6 +20,7 @@ class UserModel(Base):
 
     phone_number = Column(String(15), nullable=False, unique=True, index=True)
     email = Column(String(100), nullable=False, unique=True, index=True)
+    password = Column(String(250), nullable=False)
 
     is_verify = Column(Boolean, nullable=False, server_default=text("0"))
 
@@ -27,3 +32,10 @@ class UserModel(Base):
         back_populates="user",
         cascade="all, delete",
     )
+
+    def verify_password(self, plain_password):
+        return pwd_context.verify(plain_password, self.password)
+
+    def set_password(self, plain_password: str) -> None:
+        self.password = pwd_context.hash(plain_password)
+
