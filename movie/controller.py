f3a24d2d6f5a31d0c9589abeeee9a0cd06b663ca
dan <danial.hedaiat@gmail.com>
2026-02-13 18:08:03 +0330

    create endpoint for add favorite movie and get list of favorite movie

--- movie/controller.py
+++ movie/controller.py
@@ -3,11 +3,14 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
+from core.auth import get_current_user
 from core.database import get_db
-from movie.models import MoviesModel
-from movie.schemas import MovieResponseSchema, MovieCreateSchema, MovieUpdateSchema
+from movie.models import MoviesModel, UserFavoriteMovieModel
+from movie.schemas import MovieResponseSchema, MovieCreateSchema, MovieUpdateSchema, FavoriteMovieSchema
+from user.models import UserModel
 
 router = APIRouter(tags=["Movies"], prefix="/movies")
+
 
 @router.get("/", response_model=List[MovieResponseSchema])
 async def retrive_movie_list(db: Session = Depends(get_db)):
@@ -22,6 +25,7 @@ async def retrive_movie_detail(movie_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Movie not found")
     return movie
 
+
 @router.post("/", response_model=MovieResponseSchema)
 async def create_movie(request_movie: MovieCreateSchema, db: Session = Depends(get_db)):
     movie = MoviesModel(**request_movie.model_dump())
@@ -32,7 +36,7 @@ async def create_movie(request_movie: MovieCreateSchema, db: Session = Depends(g
 
 
 @router.put("/{movie_id}", response_model=MovieResponseSchema)
-async def update_movie(request_movie: MovieUpdateSchema,movie_id: int, db: Session = Depends(get_db)):
+async def update_movie(request_movie: MovieUpdateSchema, movie_id: int, db: Session = Depends(get_db)):
     movie = db.query(MoviesModel).filter_by(id=movie_id).first()
     if not movie:
         raise HTTPException(status_code=404, detail="Movie not found")
@@ -58,5 +62,40 @@ async def delete_movie(movie_id: int, db: Session = Depends(get_db)):
 
 
 @router.post('/favorite')
-async def add_favorite_movie(db: Session = Depends(get_db)):
-    pass
+async def add_favorite_movie(request_movie: FavoriteMovieSchema, db: Session = Depends(get_db),
+                             current_user: UserModel = Depends(get_current_user)):
+    movie = db.query(MoviesModel).filter_by(id=request_movie.movie_id).first()
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie not found")
+
+    existing = db.query(UserFavoriteMovieModel).filter_by(
+        user_id=current_user.id,
+        movie_id=request_movie.movie_id
+    ).first()
+
+    if existing:
+        raise HTTPException(status_code=400, detail="Movie already in favorites")
+
+    # Add favorite
+    favorite = UserFavoriteMovieModel(user_id=current_user.id, movie_id=request_movie.movie_id)
+    db.add(favorite)
+    db.commit()
+    db.refresh(favorite)
+    return {
+        "message": "your favorite movie successfully added",
+        "movie": movie.name,
+        "user_id": current_user.id,
+        "detail": favorite
+    }
+
+
+@router.get('/favorite/')
+async def get_favorite_movie(db: Session = Depends(get_db),
+                             current_user: UserModel = Depends(get_current_user)
+                             ):
+
+    favorites = db.query(UserFavoriteMovieModel).filter_by(user_id=current_user.id).all()
+
+    movie_names = [fav.movie.name for fav in favorites]
+
+    return {"favorite_movies": movie_names}
